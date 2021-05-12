0145a1d3a94e628cf6d6c826f545061fe0e538fe
github username <github email address>
2021-05-12 12:51:59 -0500

    started testing branch

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -4,26 +4,13 @@ import Question from "./Question.js"
 import Answer from "./Answer.js"
 
 class GamePage extends Component {
-    // state={
-    //     questions: []
-    // }
-
-    // componentDidMount(){
-    //     fetch("http://localhost:3000/api/v1/questions")
-    //     .then(res => res.json())
-    //     .then(data => this.setState({
-    //         questions: data
-    //     }))
-    // }
     
     render() {
         return(
         <div>
             <h4>Pick a gamemode!</h4>
             <div>
-                {/* {console.log(this.props.gamemode)} */}
                 {this.props.gamemodesArr.map(singleGamemode => <GameMode key={singleGamemode.id} songs={this.props.songsArr} gamemode={singleGamemode} questions={this.props.questionsArr} questionRenderChoices={this.props.questionRenderChoices} questionGetRandom={this.props.questionGetRandom} answers={this.props.answers}/>)}
-                {/* <GameMode key={this.props.gamemode.id} gamemode={this.props.gamemode} questions={this.props.questionsArr} questionRenderChoices={this.props.questionRenderChoices} answers={this.props.answers}/> */}
             </div>
         </div>)
     }

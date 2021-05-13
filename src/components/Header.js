4531ff08da5ae80bb109097bc42bf6f14c575467
github username <github email address>
2021-05-13 15:35:53 -0500

    darksizo15

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Header extends Component {
     render() {
         return(<div>
-            <h1>Welcome to Song Trivia!</h1>
+            <h1>Welcome to D&E Music Hall of Fame!</h1>
             <p> Add a welcome message and css to the header </p>
         </div>)
     }
--- src/containers/TriviaContainer.js
+++ src/containers/TriviaContainer.js
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
 import GamePage from "../components/GamePage.js"
 import Song from "../components/Song.js"
+import SongsList from "../components/SongsList.js"
 
 class TriviaContainer extends Component {
     state={
-        gamemodes: [],
-        questions: [{value: ""}],
-        answers: [{value: ""}],
-        songs: [{value: ""}],
         songInfo: ['hi'],
-        artistInfo: {
-            response: {
-                hits: [{
-                    result:{
-                        // title:'',
-                        primary_arist: {
-                            name: ""
-                        }
-                            
-                        
-                    }
-                }]
+        artistInfo: {},
+        allArtists: [
+                {
+                response: {
+                    hits: [{
+                        result:{}
+                    }]
+                }
             }
-        },
-        audio: ""
+        ],
+        artistEndpoints: [
+            "Kendrick%20Lamar",
+            "The%20Weeknd",
+            "Taylor%20Swift",
+            "6lack%20",
+            "Summer%20Walker",
+            "The Fray",
+            "Drake%20",
+            "Bryson%20Tiller",
+            "Frank%20Ocean",
+            "Doja%20Cat", 
+            "Shawn%20Mendes", 
+            "MGK%20",
+            "Ariana%20Grande",
+            "Halsey",
+            "Olivia%20Rodrigo",
+            "Lauv%20",
+            "BLACKPINK%20",
+            "LANY%20",
+            "The%201975",
+            "Sia",
+            "Bruno Mars",
+            "Dean Lewis",
+            "'Lil Wayne",
+            "Future",
+            "Luke Bryan",
+            "Maren Morris",
+            "SZA",
+            "Joji",
+            "88rising",
+            "Niki",
+            "Billie Eilish",
+            "Harry Styles",
+            "Justin Bieber",
+            "Selena Gomez",
+            "Jonas Brothers",
+            "5 Seconds of Summer",
+            "Keith Urban"
+        ]
     }
-  
 
 
     componentDidMount(){
@@ -35,45 +65,32 @@ class TriviaContainer extends Component {
             gamemodes: data
         }))
 
-        fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
-        "method": "GET",
-        "headers": {
-		"x-rapidapi-key": "0cafc7e271mshe602d6d7c0ab4dep1f3ac8jsne8fcddaeb24a",
-		"x-rapidapi-host": "genius.p.rapidapi.com"
-	    }
-        })
-        .then(res=>res.json())
-        .then(data => this.setState({
-            songInfo: data
-        }))
-        .catch(err => {
-            console.error(err);
-        });
-
-        const test = "Kendrick%20Lamar"
-
-        fetch(`https://genius.p.rapidapi.com/search?q=${test}`, {
-        "method": "GET",
-        "headers": {
-		"x-rapidapi-key": "0cafc7e271mshe602d6d7c0ab4dep1f3ac8jsne8fcddaeb24a",
-		"x-rapidapi-host": "genius.p.rapidapi.com"
-	    }
-        })
-        .then(res => res.json())
-        .then(response => this.setState({
-            artistInfo: response
-        }))
-        .catch(err => {
-            console.error(err);
-        });
+        this.state.artistEndpoints.forEach(endpoint => this.getArtist(endpoint))
     }
 
+    getArtist = (artist) => {
+        // this.componentDidMount() 
+        fetch(`https://genius.p.rapidapi.com/search?q=${artist}`, {
+            "method": "GET",
+            "headers": {
+            "x-rapidapi-key": "0cafc7e271mshe602d6d7c0ab4dep1f3ac8jsne8fcddaeb24a",
+            "x-rapidapi-host": "genius.p.rapidapi.com"
+            }
+            })
+            .then(res => res.json())
+            .then(response => this.setState({
+                allArtists: [...this.state.allArtists, response]
+            }))
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    
     render() {
-        // console.log(this.state.songInfo)
     return(
         <div>
-            <h1>TriviaContainer</h1>
-            <Song songInfo={this.state.songInfo} artistInfo={this.state.artistInfo}/>
+            {/* {console.log(this.state.allArtists)} */}
+            <SongsList songInfo={this.state.songInfo} allArtists={this.state.allArtists} artistEndpoints={this.state.artistEndpoints} artistInfo={this.state.artistInfo} getArtist={this.getArtist}/>
             {/* <GamePage gamemodesArr={this.state.gamemodes} songsArr={this.state.songs} questionsArr={this.state.questions} questionRenderChoices={this.questionRenderChoices} questionGetRandom={this.questionGetRandom} answers={this.state.answers}/> */}
         </div>
         )

225650fd20faeabfe38e7a5881b5cb5de44c3a76
github username <github email address>
2021-05-14 13:03:53 -0500

    final

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
+// class Song extends Component {
+//     removeSong =(e) => {
+//         e.target.parentElement.parentElement.innerHTML=''
+//     }
+
+//     render() {
+//       return(
+//           <div className="container songs-container" >
+
+//                   <div className="col">
+//                       <div className="card">
+//                           <h1 className="card-title">{this.props.songArray.title !== undefined ? this.props.songArray.title : null}</h1>
+//                           <img className="card-img-top" alt="..." src={this.props.songArray.song_art_image_url !== undefined ? this.props.songArray.song_art_image_url : null}></img>
+//                           <div className="card-body">
+//                               <h5 className="card-text">{this.props.songArray.full_title !== undefined ? this.props.songArray.full_title : null}</h5>
+//                               <p>Check song lyrics<a href={this.props.songArray.url !== undefined ? this.props.songArray.url : null} target="_blank" rel="noopener noreferrer"> here</a>.</p>
+//                               <button className="card-text btn btn-secondary" onClick={(e)=>this.removeSong(e)}>Remove Song</button>
+//                           </div>
+//                       </div><br/><br/>
+//                   </div>
+//               </div>
+
+//       )
+//   }
+// }
+// export default Song;
+
+import React, { Component } from 'react';
 class Song extends Component {
     removeSong =(e) => {
         e.target.parentElement.parentElement.innerHTML=''
     }
-
     render() {
       return(
-          <div className="container songs-container" >
-
-                  <div className="col">
+          <div>
                       <div className="card">
                           <h1 className="card-title">{this.props.songArray.title !== undefined ? this.props.songArray.title : null}</h1>
                           <img className="card-img-top" alt="..." src={this.props.songArray.song_art_image_url !== undefined ? this.props.songArray.song_art_image_url : null}></img>
@@ -20,8 +45,6 @@ class Song extends Component {
                           </div>
                       </div><br/><br/>
                   </div>
-              </div>
-
       )
   }
 }
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,3 +1,60 @@
+// import React, { Component } from 'react';
+// import Song from './Song.js'
+// import SongsMapped from './SongsMapped'
+// import SearchBar from './SearchBar'
+// class SongsList extends Component {
+//     state={
+//         allSongs: [],
+//         sortedSongs: []
+//     }
+
+//     componentDidMount(){
+//     const myArray = this.props.allArtists.map(artist=>artist.response.hits.map(app => app.result))
+//     const aArray = (myArray.filter(arr => arr.length > 5))
+//     const bArray = aArray.flat()
+//     this.setState({
+//         allSongs: bArray
+//         })
+//     }
+
+//     shuffle = (array) => {
+//         let currentIndex = array.length, temporaryValue, randomIndex;
+
+//         while (0 !== currentIndex) {
+//           randomIndex = Math.floor(Math.random() * currentIndex);
+//           currentIndex -= 1;
+//           temporaryValue = array[currentIndex];
+//           array[currentIndex] = array[randomIndex];
+//           array[randomIndex] = temporaryValue;
+//         }
+      
+//         return array;
+//     }
+
+//     sortArrayFx = (e) => {
+//         const sortedArray = this.state.allSongs.filter(song => 
+//             song.full_title.toLowerCase().includes(e.target.value))
+//             this.setState({ 
+//                 sortedSongs: this.shuffle(sortedArray)
+//             })
+//     }
+
+//     render(){
+//        return(
+//         <div>
+//         {console.log(this.props.allArtists)}
+//         <SearchBar filter={this.sortArrayFx}/>
+//         {this.state.sortedSongs.length > 0 ? this.state.sortedSongs.map(songA => <Song songArray={songA}/>) : this.state.allSongs.map(songB => <Song songArray={songB}/>)}
+
+
+//         </div>
+//         );
+//       }
+//     }
+
+    
+//  export default SongsList;
+
 import React, { Component } from 'react';
 import Song from './Song.js'
 import SongsMapped from './SongsMapped'
@@ -9,8 +66,6 @@ class SongsList extends Component {
     }
     // myArray = props.allArtists.map(artist=>artist.response.hits.map(app => app.result))
     // aArray = (myArray.filter(arr => arr.length > 5))
-
-
     componentDidMount(){
     const myArray = this.props.allArtists.map(artist=>artist.response.hits.map(app => app.result))
     const aArray = (myArray.filter(arr => arr.length > 5))
@@ -19,10 +74,8 @@ class SongsList extends Component {
         allSongs: bArray
         })
     }
-
     shuffle = (array) => {
         let currentIndex = array.length, temporaryValue, randomIndex;
-
         while (0 !== currentIndex) {
           randomIndex = Math.floor(Math.random() * currentIndex);
           currentIndex -= 1;
@@ -30,10 +83,8 @@ class SongsList extends Component {
           array[currentIndex] = array[randomIndex];
           array[randomIndex] = temporaryValue;
         }
-      
         return array;
     }
-
     sortArrayFx = (e) => {
         const sortedArray = this.state.allSongs.filter(song => 
             song.full_title.toLowerCase().includes(e.target.value))
@@ -42,21 +93,22 @@ class SongsList extends Component {
             })
     }
 
+    // clickHandle = () => {
+    //     {console.log(this.state.allSongs)}
+    // }
     render(){
        return(
         <div>
-        {/* {console.log(this.props.allArtists)} */}
+            {/* <button onCLick={this.clickHandle}>Show me the money</button> */}
+            {/* {console.log(this.state.allSongs)} */}
         <SearchBar filter={this.sortArrayFx}/>
-        {/* <SongsMapped array={this.state.sortedSongs.length > 0 ? this.state.sortedSongs : this.state.allSongs}/> */}
+        <div className="row">
+        <div className="container col-20 d-flex p-8" style={{flexWrap: 'wrap'}}>
         {this.state.sortedSongs.length > 0 ? this.state.sortedSongs.map(songA => <Song songArray={songA}/>) : this.state.allSongs.map(songB => <Song songArray={songB}/>)}
-
-
-        {/* ***working code*** */}
-        {/* {aArray[1] != undefined ? (aArray.map(arr => <SongsMapped array={arr}/>)) : null} */}
+        </div>
+        </div>
         </div>
         );
       }
     }
-
-    
  export default SongsList;
